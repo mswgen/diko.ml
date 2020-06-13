2eb3cdad2829317dff20f106bb489bd3135c6860
mswgen <54498184+mswgen@users.noreply.github.com>
2020-06-13 12:35:24 +0900

    Create koreanbots.js

---
+++ commands/koreanbots.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'koreanbots',
+    aliases: ['hellothisisverification'],
+    description: 'koreanbots 봇 소유자 확인을 위한 명령어에요.',
+    category: 'other',
+    usage: '!hellothisisverification',
+    run: async (client, message, args, option) => {
+        message.channel.send(`${client.users.cache.get('647736678815105037').tag}(${client.users.cache.get('647736678815105037').id})`);
+    }
+}
