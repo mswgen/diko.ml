d6ce473499ee3afb529425300edfd165bb8bdbf3
mswgen <54498184+mswgen@users.noreply.github.com>
2020-06-30 19:06:19 +0900

    핑 명령어 추가

--- commands/koreanbots.js
+++ commands/koreanbots.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'koreanbots',
     aliases: ['hellothisisverification'],
     description: 'koreanbots 봇 소유자 확인을 위한 명령어에요.',
-    category: 'other',
+    category: 'info',
     usage: 'd!hellothisisverification',
     run: async (client, message, args, db) => {
         message.channel.send(`${client.users.cache.get('647736678815105037').tag}(${client.users.cache.get('647736678815105037').id})`);
